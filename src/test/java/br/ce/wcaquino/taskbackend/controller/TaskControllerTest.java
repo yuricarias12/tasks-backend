c48e05627aba0beb9fe8914e54f0c5a46a967e7e
Carias <yuristricken7@gmail.com>
2025-01-27 08:20:54 -0300

    Testes unitários da aplicação

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,84 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import java.time.LocalDate;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+
+public class TaskControllerTest {
+	
+	@Mock
+	private TaskRepo taskRepo;
+	
+	@InjectMocks
+	TaskController controller;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefasSemDescricao() {
+		Task todo = new Task();
+		todo.setDueDate(LocalDate.now());
+		
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+			
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the task description", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefasSemData() {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		//todo.setDueDate(LocalDate.now());
+		TaskController controller = new TaskController();
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the due date", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefasComDataPassada() {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.of(2010, 01, 01));
+		
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Due date must not be in past", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void deveSalvarTarefasComSucesso() throws ValidationException {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.now());
+		controller.save(todo);
+		
+		Mockito.verify(taskRepo).save(todo);
+			
+	}
+
+}
